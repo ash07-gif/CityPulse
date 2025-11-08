f38e640aaf822c35cd381a17ee248a15e34d647b
Ash Porob <ashporob@gmail.com>
2025-11-08 17:46:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1106152726-853ba",
+  "appId": "1:426167907101:web:6e7c6c5c71557ecebea7f4",
+  "apiKey": "AIzaSyCFSlZugesa1C586oeDXOJpRg1nJxx1VEY",
+  "authDomain": "studio-1106152726-853ba.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "426167907101"
+};
